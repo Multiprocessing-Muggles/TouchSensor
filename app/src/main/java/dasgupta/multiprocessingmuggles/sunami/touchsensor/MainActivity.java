f7aef1b01a10fd3b23da248a3a8a7126a51fb9a7
sunami09 <sunamidasgupta@gmail.com>
2022-04-02 23:33:02 +0530

    Intial Commit

---
+++ app/src/main/java/dasgupta/multiprocessingmuggles/sunami/touchsensor/MainActivity.java
@@ -0,0 +1,14 @@
+package dasgupta.multiprocessingmuggles.sunami.touchsensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
